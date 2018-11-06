773e5214587729af202d728c49caba60689438ce
meyer0656 <brandon.meyer@ufl.edu>
2018-11-06 00:43:29 -0500

    Finished product.

---
+++ js/pct.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  $('#1').on('click', () => {
+    $('#large').attr('src', 'images/1.JPG');
+  })
+  $('#2').on('click', () => {
+    $('#large').attr('src', 'images/2.JPG');
+  })
+  $('#3').on('click', () => {
+    $('#large').attr('src', 'images/3.JPG');
+  })
+  $('#4').on('click', () => {
+    $('#large').attr('src', 'images/4.JPG');
+  })
+  $('#5').on('click', () => {
+    $('#large').attr('src', 'images/5.JPG');
+  })
+  $('#6').on('click', () => {
+    $('#large').attr('src', 'images/6.JPG');
+  })
+  $('#7').on('click', () => {
+    $('#large').attr('src', 'images/7.JPG');
+  })
+  $('#8').on('click', () => {
+    $('#large').attr('src', 'images/8.JPG');
+  })
+  $('#9').on('click', () => {
+    $('#large').attr('src', 'images/9.JPG');
+  })
+  $('#10').on('click', () => {
+    $('#large').attr('src', 'images/10.JPG');
+  })
+});
